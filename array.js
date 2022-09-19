d12b663d8d6266783e872f8d9ae2a6eaf6da48a4
MarcoPereiraIEFP <marco_mrcoi@hotmail.com>
2022-09-19 15:01:58 +0100

    unfinished
    Cannot seem to make loop run through button click

---
+++ array.js
@@ -0,0 +1,14 @@
+let names = ["Bruce", "Clark", "Diana", "Arthur", "Barry"];
+var pos = 0;
+
+function iter_right() {
+    for(var i = 0; names[pos].length < 5; i++) {
+
+    }    
+
+}
+
+
+document.getElementById("arrayDisplay").innerHTML = names[pos]
+document.getElementById("buttonRight").onclick = iter_right();
+  
